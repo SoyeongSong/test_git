50208aa4a8f41ecfd70e01a9063007b9074b3ece
Your Name <ssy0316@gmail.com>
2019-08-20 11:50:50 +0900

    commit

--- src/main/java/com/example/test_spbt2/controller/MainController.java
+++ src/main/java/com/example/test_spbt2/controller/MainController.java
@@ -10,4 +10,41 @@ public class MainController {
 public String main(){
 return "main";
 }
+
+@RequestMapping(value = "/user/login", method=RequestMethod.GET)
+public String memberAction5(){
+    return "/user/login";
+}
+
+@RequestMapping(value = "/user/logout", method=RequestMethod.GET)
+public String memberAction6(){
+    return "/user/logout";
+}
+
+@RequestMapping(value = "/user/signup", method=RequestMethod.GET)
+public String memberAction7(){
+    return "/user/signup";
+}
+
+// @RequestMapping(value = "/member/list", method=RequestMethod.GET)
+// public String memberAction1(){
+//     return "/member/list";
+// }
+
+
+// @RequestMapping(value = "/member/edit", method=RequestMethod.GET)
+// public String memberAction2(){
+//     return "/member/edit";
+// }
+
+// @RequestMapping(value = "/member/delete", method=RequestMethod.GET)
+// public String memberAction3(){
+//     return "/member/delete";
+// }
+
+// @RequestMapping(value = "/member/view", method=RequestMethod.GET)
+// public String memberAction4(){
+// return "/member/view";
+// }
+
 }
