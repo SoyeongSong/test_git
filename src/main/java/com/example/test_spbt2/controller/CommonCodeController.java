6d8f50a8e26e6c0c0ee6b1665249462cbef10897
Your Name <ssy0316@gmail.com>
2019-08-21 17:05:21 +0900

    commit

--- src/main/java/com/example/test_spbt2/controller/CommonCodeController.java
+++ src/main/java/com/example/test_spbt2/controller/CommonCodeController.java
@@ -1,19 +1,45 @@
 package com.example.test_spbt2.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping(value = "/commonCode")
-
 public class CommonCodeController {
-@RequestMapping(value = "/edit", method={RequestMethod.GET, RequestMethod.POST})
-    public void edit(){
-        
+    @RequestMapping(value = "/commonCode/{action}")
+    public ModelAndView edit(@RequestParam Map<String, Object> paramMap, @PathVariable String action, ModelAndView modelandView) {
+        String viewName = "/commonCode/";
+
+        Map<String, Object> resultMap = new HashMap<String, Object>();
+
+        if ("read".equals(action)) {
+            viewName = viewName + action;
+            resultMap = paramMap;
+        }
+
+        List<Object> resultList = new ArrayList<Object>();
+
+        if ("list".equals(action)) {
+            viewName = viewName + action;
+            Map<String, Object> data01 = new HashMap<String, Object>();
+            data01.put("NAME", "name 01");
+            data01.put("COMMON_CODE_ID", "490293232849");
+            data01.put("DESCRIPTION", "anything DESCRIPTION 01");
+            data01.put("USERNO", "00000001");
+            resultList.add(data01);
+        }
+
+        modelandView.setViewName(viewName);
+        modelandView.addObject("resultMap", resultMap);
+        modelandView.addObject("resultList", resultList);
+
+        return modelandView;
     }
-
 }
-
-
-
